ac4827ce26c4510952f931a844d68123d8601769
KLAI MAROUAN <49674822+Marouankl@users.noreply.github.com>
2023-11-06 22:28:45 +0100

    create login and update

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+import MySQLdb
 from _curses import *
 
 import os
-from flask import Flask, render_template, jsonify, json, request, redirect, url_for
+from flask import Flask, render_template, jsonify, json, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import collections
 
 app = Flask(__name__)
-
+app.secret_key = 'your secret key'
 # MySQL configurations
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
@@ -15,11 +16,40 @@ app.config['MYSQL_DB'] = "apirestpython"
 mysql = MySQL(app)
 
 
-@app.route('/')
+@app.route('/home')
 def hello_world():
     return render_template('home.html')
 
-#
+
+#------
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+#----------------------------------------------------------------------------------------------------------------------------------------
+@app.route('/login')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = % s AND password = % s', (email, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['email'] = account['email']
+            msg = 'Logged in successfully !'
+            return render_template('home.html', msg=msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg=msg)
+
+
 #----------------------------------------------------------------------------------------------------------------------------------------
 #Get pokemons with fatchall
 #1) GET - /api/pokemons : Récupère la liste de tous les pokémons:
@@ -27,7 +57,7 @@ def hello_world():
 def getPokemon():
     conn = mysql.connection.cursor()
     conn.execute(
-        'SELECT p.id_Pokemon, p.name, p.size, p.weight, p.Statistics, p.picture '
+        'SELECT p.id_Pokemon, p.name, p.size, p.weight, p.statistical, p.picture '
         'FROM pokemon p '
                 )
     result = conn.fetchall()
@@ -40,7 +70,7 @@ def getPokemon():
 @app.route('/api/pokemons/<int:id>', methods=['GET'])
 def getPokemonById(id):
     conn = mysql.connection.cursor()
-    conn.execute('SELECT p.id_Pokemon, p.name, p.size, p.weight, p.Statistics, p.picture, s.powers, s.description, s.precisions, s.PPMax, t.typeName '
+    conn.execute('SELECT p.id_Pokemon, p.name, p.size, p.weight, p.statistical, p.picture, s.powers, s.description, s.precisions, s.PPMax, t.typeName '
         'FROM pokemon p '
         'INNER JOIN fastSkils fs ON p.id_Pokemon = fs.id_Pokemon '
         'JOIN skils s ON fs.id_Competance = s.id_Competance '
@@ -67,7 +97,7 @@ def getTypeById(id):
 
 #----------------------------------------------------------------------------------------------------------------------------------------
 # 4) GET - /api/abilities : Récupère la liste de toutes les compétences
-@app.route('/api/abilities')
+@app.route('/api/abilities', methods=['GET'])
 def getSkils():
     conn = mysql.connection.cursor()
     conn.execute(
@@ -97,43 +127,46 @@ def getSkils():
     else:
         return "Compétences non trouvées"
 
-
-
-#----------------------------------------------------------------------------------------------------------------------------------------
-# 10) DELETE - /api/pokemons/:id : Supression du pokémon précisé par :id
-# La methode pour supprimer un pokemon avec son id
-@app.route('/api/pokemons/delete/<int:id>', methods=['GET', 'POST'])
-def deleteTask(id):
-    conn = mysql.connection.cursor()  # Pour connecter à la base de données
-    conn.execute('DELETE FROM pokemon WHERE id_Pokemon=%s', [id])  # Requête SQL pour supprimer un Pokémon avec un ID
-    conn.connection.commit()
-
-    # Rediriger l'utilisateur vers la page 'pokemon.html'
-    return redirect(url_for('getPokemon'))
-
 #----------------------------------------------------------------------------------------------------------------------------------------
 #5)POST - /api/pokemons : Ajout d’un pokémon
 #creaded new pokemon
 @app.route('/api/pokemons/create', methods=['POST'])
 def createPokemon():
-
     if request.method == 'POST':
         conn = mysql.connection.cursor()
         name = request.form['name']
         size = request.form['size']
         weight = request.form['weight']
-        Statistics = request.form['Statistics']  # Utilisez une minuscule pour le nom de la variable
+        statistical = request.form['statistical']
         picture = request.form['picture']
-        sql = "INSERT INTO pokemon (name, size, weight, Statistics, picture) VALUES (%s, %s, %s, %s, %s)"
-        data = (name, size, weight, Statistics, picture)
+        powers = request.form['powers']
+        description = request.form['description']
+        precisions = request.form['precisions']
+        PPMax = request.form['PPMax']
+        typeName = request.form['typeName']
+
+        sql = 'INSERT INTO pokemon (name, size, weight, statistical, picture) VALUES (%s, %s, %s, %s, %s)'
+        data = (name, size, weight, statistical, picture)
         conn.execute(sql, data)
-        # return le résultat  en forma  JSON response
-        return redirect(url_for('pokemon.html'))
+
+        idPokemon = conn.lastrowid
+
+        sql = 'SELECT id_Type FROM type WHERE typeName = %s'
+        conn.execute(sql, (typeName,))
+        type_id = conn.fetchone()
+
+        sql = 'INSERT INTO skils  (powers, description, precisions, PPMax, id_Type) VALUES (%s, %s, %s, %s, %s)'
+        data = (powers, description, precisions, PPMax, type_id)
+        conn.execute(sql, data)
+
+        sql = 'INSERT INTO fastSkils  (id_Pokemon, id_Competence) VALUES (%s, %s)'
+        data = (idPokemon, conn.lastrowid)
+        conn.execute(sql, data)
+
+        conn.connection.commit()
+        return redirect(url_for('getPokemon'))
     else:
-        return ('error')
-
-
-
+        return 'error'
 
 #----------------------------------------------------------------------------------------------------------------------------------------
 #creaded new pokemon
@@ -143,8 +176,8 @@ def createTypes():
     if request.method == 'POST':
         conn = mysql.connection.cursor()
         sql = (
-            'INSERT INTO type (id_Type,typeName, id_Competance,powers, description, precisions, PPMax,id_Pokmeon, name, size, weight, Statistics, picture) '
-            'SELECT t.typeName, s.powers, s.description, s.precisions, s.PPMax, p.name, p.size, p.weight, p.Statistics, p.picture '
+            'INSERT INTO type (id_Type,typeName, id_Competance,powers, description, precisions, PPMax,id_Pokmeon, name, size, weight, statistics, picture) '
+            'SELECT t.typeName, s.powers, s.description, s.precisions, s.PPMax, p.name, p.size, p.weight, p.statistics, p.picture '
             'FROM Type t '
             'LEFT JOIN Skils s ON t.id_Type = s.id_Competance '
             'INNER JOIN FastSkils fs ON fs.id_Pokemon = t.id_Pokemon '
@@ -155,35 +188,107 @@ def createTypes():
         conn.close()
 
         # Return a JSON response to indicate success
-        return json.dumps({'message': 'Votre tache bien ajoutée !'})
+        return render_template('createType.html')
     else:
         return 'error'
 
+
+
+
+
 #----------------------------------------------------------------------------------------------------------------------------------------
-@app.route('/api/pokemons/update/<int:id>', methods=['PUT' ])
+#7)PUT - /api/pokemons/:id : Modification du pokémon précisé par :id
+@app.route('/api/pokemons/update/<int:id>', methods=['GEt','POST' ])
 def UpdateForm(id):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    if request.method == 'PUT':
-        id_Pokemon = request.form['id_Pokemon']
-        name = request.form['name']
-        size = request.form['size']
-        weight = request.form['weight']
-        statistics = request.form['statistics']
-        picture = request.form['picture']
-        sql = ('UPDATE pokemon '
-            'SET name=%s, size=%s, weight=%s, Statistics=%s, picture=%s '
-            'WHERE id_Pokemon=%s')
-        data = (id_Pokemon,name, size, weight, statistics, picture, id)
-        cursor.execute(sql, data)
-        conn.commit()
-        # return le résultat  en forma  JSON response
-        return render_template('updatePokemon.html', id=id)
+    if request.method == 'POST':
+        conn = mysql.connection.cursor()
+        name = request.form.get('name')
+        size = request.form.get('size')
+        weight = request.form.get('weight')
+        statistics = request.form.get('statistics')
+        picture = request.form.get('picture')
+
+        sql = (
+            'UPDATE pokemon '
+            'SET name=%s, size=%s, weight=%s, statistics=%s, picture=%s '
+            'WHERE id_Pokemon=%s'
+        )
+        data = (name, size, weight, statistics, picture, id)
+
+        conn.execute(sql, data)
+        conn.connection.commit()
+        conn.close()
+
+        # Return a success message or redirect to a page
+        flash('Pokemon updated successfully', 'success')
+        return render_template('pokemon.html', id=id)
     else:
-        return ('error')
+        # You can render an update form for editing here
+        return render_template('updatePokemon.html', id=id)
 
+# ----------------------------------------------------------------------------------------------------------------------------------------
+#8)PUT - /api/abilities/:id : Modification de la compétence précisé par :id
+@app.route('/api/abilities/update/<int:id>', methods=['GEt','POST' ])
+def Updateabilities(id):
+    if request.method == 'POST':
+        conn = mysql.connection.cursor()
+        powers = request.form.get('powers')
+        description = request.form.get('description')
+        precisions = request.form.get('precisions')
+        PPMax = request.form.get('	PPMax')
 
+        sql = (
+            'UPDATE skils '
+            'SET powers=%s, description=%s, precisions=%s, PPMax=%s '
+            'WHERE id_Competance=%s'
+        )
+        data = (powers, description, precisions, PPMax,  id)
 
+        conn.execute(sql, data)
+        conn.connection.commit()
+        conn.close()
+
+        # Return a success message or redirect to a page
+        flash('Skils updated successfully', 'success')
+        return render_template('pokemon.html', id=id)
+    else:
+        # You can render an update form for editing here
+        return render_template('updateAbilities.html', id=id)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------
+#9. PUT - /api/type/:id : Modification du type précisé par :id
+@app.route('/api/type/update/<int:id>', methods=['GEt', 'POST'])
+def Updatetype(id):
+    if request.method == 'POST':
+        conn = mysql.connection.cursor()
+        typeName = request.form.get('typeName')
+        sql = (
+            'UPDATE type '
+            'SET typeName=%s '
+            'WHERE id_Type=%s'
+        )
+        data = (typeName, id)
+        conn.execute(sql, data)
+        conn.connection.commit()
+        conn.close()
+
+        # Return a success message or redirect to a page
+        flash('Type updated successfully', 'success')
+        return render_template('pokemon.html', id=id)
+    else:
+        # You can render an update form for editing here
+        return render_template('updateType.html', id=id)
+#----------------------------------------------------------------------------------------------------------------------------------------
+# 10) DELETE - /api/pokemons/:id : Supression du pokémon précisé par :id
+# La methode pour supprimer un pokemon avec son id
+@app.route('/api/pokemons/delete/<int:id>', methods=['POST'])
+def deleteTask(id):
+    conn = mysql.connection.cursor()  # Pour connecter à la base de données
+    conn.execute('DELETE FROM pokemon WHERE id_Pokemon=%s', [id])  # Requête SQL pour supprimer un Pokémon avec un ID
+    conn.connection.commit()
+
+    # Rediriger l'utilisateur vers la page 'pokemon.html'
+    return redirect(url_for('getPokemon'))
 
 
 #-------------------------------
@@ -191,7 +296,9 @@ def UpdateForm(id):
 @app.route('/api/pokemons/create', methods=['GET'])
 def createPokemons():
     return render_template('createPokemon.html')
-
+# @app.route('/api/pokemons/update/<id>', methods=['GET'])
+# def updatePokemons(id):
+#     return render_template('updatePokemon.html' , id=id)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
